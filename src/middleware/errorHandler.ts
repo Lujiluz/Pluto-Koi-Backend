49c8573eedd0afc57c992a2e7301e4258bf7cd74
Lujiluz <zlfjri@gmail.com>
2025-09-28 00:10:15 +0700

    initial commit

---
+++ src/middleware/errorHandler.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from "express"
+class CustomErrorHandler extends Error {
+    statusCode: number
+    message: string
+
+    constructor(statusCode: number, message: string) {
+        super()
+        this.statusCode = statusCode
+        this.message = message
+    }
+}
+
+const errorHandler = (err: CustomErrorHandler, req: Request, res: Response, next: NextFunction) => {
+    const { statusCode, message } = err
+    
+    res.status(statusCode || 500).json({
+        status: 'error',
+        statusCode,
+        message
+    })
+}
